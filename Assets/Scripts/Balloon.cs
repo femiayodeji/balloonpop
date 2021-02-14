82dfc1ffa16bf8689a8a69d08c18e813d617935a
femi <femidotayodeji@gmail.com>
2021-02-14 15:12:04 +0100

    Added balloon burst action

---
+++ Assets/Scripts/Balloon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+}
