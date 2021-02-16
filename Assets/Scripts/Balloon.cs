ed14a4558f0330c977e0e170886c92b2aaaa264f
Femi Ayodeji <femidotayodeji@gmail.com>
2021-02-16 16:27:37 +0100

    work in progress

---
+++ Assets/Scripts/Balloon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+    public AudioSource burstSound;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        GameObject.Find("GameManager").GetComponent<GameManager>().ScoreUp();        
+        burstSound.Play();
+        Destroy(gameObject);
+    }
+}
